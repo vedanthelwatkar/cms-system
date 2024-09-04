587d08f43fc05ca2d64bf7bc73ccc21e0597ebcc
Vedant Helwatkar <vedant@unifynd.com>
2024-09-04 15:48:21 +0530

    changed model

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -3,6 +3,13 @@ const mongoose = require("mongoose");
 const AdminSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  username: {
+    type: String,
+    unique: true,
+    default: function () {
+      return this.email.split("@")[0];
+    },
+  },
 });
 
 module.exports = mongoose.model("Admin", AdminSchema);
